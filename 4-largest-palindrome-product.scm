08003dfcf7220eb21319839abaecc42bc01a4257
Blair Drummond <bdrum047@uottawa.ca>
2015-03-05 17:32:54 -0500

    Finished number 4

--- 4-largest-palindrome-product.scm
+++ 4-largest-palindrome-product.scm
@@ -1,21 +1,14 @@
+;;; find largest palindrome below k
 
-(define (range a b) (if (eqv? a b) () (cons a (range (+ a 1) b)) ))
-(define (products k) (let ((l (range 1 k))) 
-		       (sort (append-map (lambda (x) (map (lambda (y) (* x y)) l)) l) >)))
+(define (find-palindrome k) (find-recurse k k))
 
-(define (duplicates l) (let ((tail (cdr l)))
-			 (cond 
-			  ((null? l)               `()                      )
-			  ((null? tail)             l                       ) 
-			  ((eq? (car l) (cadr l))   (duplicates tail)       )
-			  (else                     (cons (car l) (duplicates tail))))))
-
-;;; This is actually in reverse, but that isn't important for our purposes
 (define (numToList n)  (if  (< n 10) (cons n ()) (cons (remainder n 10)  (numToList (truncate (/ n 10))))))
 
-(define (find-palin k) (find-recurse (duplicates (products k))))
+(define (palindrome a) (let ((l (numToList a))) 
+			 (equal? l (reverse l))))
 
-(define (find-recurse l) (if (palindrome (car l))  l  (find-recurse (cdr l))))
-
-(define (palindrome a) (let ((l (numToList a))) (
-			 (eqv? l (reverse l)))))
+(define (find-recurse a b) (if (palindrome (* a b)) 
+			      (* a b)  
+			      (if (< a b)   
+				  (find-recurse a       (- b 1) )  
+				  (find-recurse (- a 1) b       ))))
