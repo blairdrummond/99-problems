65fa1c210f869d8285d3e07ce766cffb89a03506
Your Name <you@example.com>
2015-07-11 12:50:27 -0400

    Actually fixed?

--- 18-maximum-path-sum.rkt
+++
@@ -1,5 +0,0 @@
-(define (fact n) (if (= n 0) 1 (* n (fact (- n 1)))))
-
-(define (decomp n) (if (< n 10) (list n) (cons (modulo n 10) (decomp (quotient n 10)))))
-
-(define (q18 n) (foldl + 0 (decomp (fact n))))
--- 6-sum-square-difference.hs
+++
@@ -1,4 +0,0 @@
-import Data.List 
-
-difference :: Integer  -> Integer
-difference n =  (foldl' (+) 0 [1..n])^2 - (foldl' (\ x y -> x + y^2) 0 [1..n])
